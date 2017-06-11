1cae33428f6371e3724981424d5deb18a5d6ef41
yogeshSharma0201 <yogesh02011998@gmail.com>
2017-06-11 18:02:16 +0530

    made changes for heroku

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on PORT '+port);
+  console.log('Express server is up on PORT '+PORT);
 });
